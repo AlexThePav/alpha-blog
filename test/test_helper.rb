40b630770863888f7de7ffe7c22b9f1c3c2bb4dc
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2019-01-15 22:00:29 +0200

    Fix tests and update navbar with categories

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,6 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def sign_in_as(user, password)
     post login_path, params: {session: {email: user.email, password: password}}
+    # post login_path, session: {email: user.email, password: password} # in Rails 4
   end
 end
